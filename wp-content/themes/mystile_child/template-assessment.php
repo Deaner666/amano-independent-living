fd23a9d80db20921656c0f3ff3e55142e8a08889
Dave Dean <dave@moortor-design.co.uk>
2014-11-26 14:03:59 +0000

    Added online assessment template.

--- wp-content/themes/mystile_child/template-assessment.php
+++ wp-content/themes/mystile_child/template-assessment.php
@@ -19,96 +19,99 @@ global $woo_options;
 get_header();
 ?>
 
-    <div id="content" class="col-full">
+    <div id="content" class="page col-full">
+
+        <?php woo_main_before(); ?>
     	
-    	<?php woo_main_before(); ?>
-    
 		<section id="main" class="col-left">
 
-            <article id="contact-page" class="page">
+            <?php
+                if ( is_front_page() ) { ?>
 
-            <?php if( isset( $emailSent ) && $emailSent == true ) { ?>
+                <article class="full-width-feature light" id="independent-living">
+                    <h1>Independent Living</h1>
+                    <h2>Enable your loved-one to live independently in their own home. Safely and on their own terms.</h2>
+                    <p>Our range of simple in-home sensors and detectors work without any intervention.</p>
+                    <img src="wp-content/themes/mystile_child/images/icons/amano-connect-main-icon.png" alt="" />
+                    
+                </article>
 
-                <p class="info"><?php _e( 'Your email was successfully sent.', 'woothemes' ); ?></p>
+                <div id="home-page-lede-buttons">
+                    <div id="help-me-choose" class="feature-button">
+                        <a href="/help-advice">Help Me Choose the Right Products</a>
+                    </div>
+                    <div id="ready-to-shop" class="feature-button">
+                        <a href="/shop">I Know What I Want. <br />I'm Ready to Shop</a>
+                    </div>
+                </div>
 
-            <?php } else { ?>
+                <article class="full-width-feature dark" id="your-peace-of-mind">
+                    <h1>Your Peace of Mind</h1>
+                    <h2>No more worrying about your loved-one’s wellbeing. No more panicky visits to check they’re OK.</h2>
+                    <p>We provide the reassurance that your loved-one is safe and healthy so you can enjoy your visits in a better frame of mind.</p>
+                </article>
 
-                <?php if ( have_posts() ) { ?>
+                <article class="full-width-feature light" id="connections-diagram">
+                    <img src="wp-content/themes/mystile_child/images/icons/connections-icon.png" alt="" />
+                </article>
 
-                <?php while ( have_posts() ) { the_post(); ?>
+                <article class="full-width-feature dark" id="making-connections">
+                    <h1>Making Connections</h1>
+                    <h2>We keep you connected to your loved-one and your loved-one connected to the rest of the world.</h2>
+                    <p>Our monitoring products alert you if something is up. Our simple to use computing devices help your loved-one connect online.</p>
+                </article>
+
+                <article class="full-width-feature light" id="preserving-dignity">
+                    <h1>Preserving Dignity</h1>
+                    <h2>Help maintain quality of life. Avoid expensive residential care costs.</h2>
+                    <p>We’ll help you choose products that cater to your loved-one’s care requirements, keeping them in their own home and out of costly care homes.</p>
+                </article>
+
+                <div id="home-page-buttons">
+                    <ul>
+                        <li><a href="" id="choose-button">Help Me Choose the Right Products</a></li>
+                        <li><a href="" id="buy-button">Buy Products</a></li>
+                        <li><a href="" id="support-button">Our Support<br />Services</a></li>
+                    </ul>
+                </div>
+
+            <?php }
+
+            	if ( have_posts() ) { $count = 0;
+            		while ( have_posts() ) { the_post(); $count++;
+            ?>                                                           
+            <article <?php post_class(); ?>>
+				
+				<?php
+                    if (!is_front_page()) { ?>
+                        <header>
+        			    	<h1><?php the_title(); ?></h1>
+        				</header>
+                    <?php }
+                ?>
+
+                <section class="entry">
+                	<?php the_content(); ?>
+
+					<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'woothemes' ), 'after' => '</div>' ) ); ?>
+               	</section><!-- /.entry -->
                 
-                		<header>
-                			<h1><?php the_title(); ?></h1>
-                		</header>
+            </article><!-- /.post -->
+            
+            <?php
+            	// Determine wether or not to display comments here, based on "Theme Options".
+            	if ( isset( $woo_options['woo_comments'] ) && in_array( $woo_options['woo_comments'], array( 'page', 'both' ) ) ) {
+            		comments_template();
+            	}
 
-                        <section class="entry">
-                			
-                			<div class="location-twitter fix">
-    							<?php if ( isset( $woo_options['woo_contact_panel'] ) && $woo_options['woo_contact_panel'] == 'true' ) { ?>
-						    	<section id="office-location">
-									<?php if (isset($woo_options['woo_contact_title'])) { ?><h3><?php echo esc_html( $woo_options['woo_contact_title'] ); ?></h3><?php } ?>
-									<ul>
-										<?php if (isset($woo_options['woo_contact_title']) && $woo_options['woo_contact_title'] != '' ) { ?><li><?php echo nl2br( esc_html( $woo_options['woo_contact_address'] ) ); ?></li><?php } ?>
-										<?php if (isset($woo_options['woo_contact_number']) && $woo_options['woo_contact_number'] != '' ) { ?><li><?php _e('Tel:','woothemes'); ?> <?php echo esc_html( $woo_options['woo_contact_number'] ); ?></li><?php } ?>
-										<?php if (isset($woo_options['woo_contact_fax']) && $woo_options['woo_contact_fax'] != '' ) { ?><li><?php _e('Fax:','woothemes'); ?> <?php echo esc_html( $woo_options['woo_contact_fax'] ); ?></li><?php } ?>
-										<?php if (isset($woo_options['woo_contactform_email']) && $woo_options['woo_contactform_email'] != '' ) { ?><li><?php _e('Email:','woothemes'); ?> <a href="mailto:<?php echo $woo_options['woo_contactform_email']; ?>"><?php echo esc_html( $woo_options['woo_contactform_email'] ); ?></a></li><?php } ?>
-									</ul>
-						    	</section>
-						    	<?php } ?>
-
-						    	<div class="contact-social<?php if ( ( isset( $woo_options['woo_contact_panel'] ) && $woo_options['woo_contact_panel'] == 'true' ) && ( ( isset( $woo_options['woo_contact_twitter'] ) && $woo_options['woo_contact_twitter'] != '' ) || ( isset($woo_options['woo_contact_subscribe_and_connect']) && $woo_options['woo_contact_subscribe_and_connect'] == 'true' ) ) ) { ?> col-right<?php } ?>">
-						    		
-						    		<!-- Begin MailChimp Signup Form -->
-									<div id="mc_embed_signup">
-										<h3>Join us</h3>
-										<p class="subscribe-intro">Join our email newsletter for exclusive deals and the latest telecare news from Amano Connect</p>
-										<form action="//amanotech.us4.list-manage.com/subscribe/post?u=2717d4782e67bd897fbe8cf26&amp;id=c2d5ad4e35" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-											<div class="mc-field-group">
-												<label for="mce-EMAIL">Email Address *</label>
-												<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-											</div>
-											<div class="mc-field-group">
-												<label for="mce-FNAME">First Name</label>
-												<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
-											</div>
-											<div class="mc-field-group">
-												<label for="mce-LNAME">Last Name</label>
-												<input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
-											</div>
-											<div id="mce-responses" class="clear">
-												<div class="response" id="mce-error-response" style="display:none"></div>
-												<div class="response" id="mce-success-response" style="display:none"></div>
-											</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-										    <div style="position: absolute; left: -5000px;"><input type="text" name="b_2717d4782e67bd897fbe8cf26_c2d5ad4e35" value=""></div>
-											<div class="clear"><input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-										</form>
-									</div><!--End mc_embed_signup-->
-
-						    		<?php if ( isset($woo_options['woo_contact_subscribe_and_connect']) && $woo_options['woo_contact_subscribe_and_connect'] == 'true' ) { woo_subscribe_connect(); } ?>
-						    	
-						    	</div>
-
-						    	<hr class="contact-separator" />
-
-						    	<?php the_content(); ?>
-						    	
-						    </div><!-- /.location-twitter -->
-	                        
-                        </section>
-                        
-                        <?php if ( isset($woo_options['woo_contactform_map_coords']) && $woo_options['woo_contactform_map_coords'] != '' ) { $geocoords = $woo_options['woo_contactform_map_coords']; }  else { $geocoords = ''; } ?>
-                		<?php if ($geocoords != '') { ?>
-                		<?php woo_maps_contact_output("geocoords=$geocoords"); ?>
-                		<?php echo do_shortcode( '[hr]' ); ?>
-                		<?php } ?>
-
-                    <?php
-                    		} // End WHILE Loop
-                    	}
-                    }
-                    ?>
-
-            </article><!-- /#contact-page -->
+				} // End WHILE Loop
+			} else {
+		?>
+			<article <?php post_class(); ?>>
+            	<p><?php _e( 'Sorry, no posts matched your criteria.', 'woothemes' ); ?></p>
+            </article><!-- /.post -->
+        <?php } // End IF Statement ?>  
+        
 		</section><!-- /#main -->
 		
 		<?php woo_main_after(); ?>
@@ -116,5 +119,5 @@ get_header();
         <?php get_sidebar(); ?>
 
     </div><!-- /#content -->
-
+		
 <?php get_footer(); ?>
