793014421d3af1a22c31d7dc7e3ed87d787d9eda
Dave Dean <dave@moortor-design.co.uk>
2014-12-10 09:01:42 +0000

    Returning to old slider shortcode.

--- wp-content/themes/mystile_child/page.php
+++ wp-content/themes/mystile_child/page.php
@@ -90,8 +90,7 @@ if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename(
                 </article>
 
                 <article class="full-width-feature light">
-                    <?php //echo do_shortcode( '[wpb-latest-product title="Latest Products"]' ) ?>
-                    <?php echo do_shortcode("[wcps id='415' ]"); ?>
+                    <?php echo do_shortcode( '[wpb-latest-product title="Latest Products"]' ) ?>
                 </article>
 
                 <article class="full-width-feature light" id="your-peace-of-mind">
